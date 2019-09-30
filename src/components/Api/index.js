694f7e2dc97b0fd348068a221cf647622d1c2c84
Pato <pato@grayshirts.com.ar>
2019-09-30 16:36:05 -0300

    added express server

---
+++ src/components/Api/index.js
@@ -0,0 +1,32 @@
+const index = require('express').Router();
+const request = require('request');
+
+index.get('/holis/', (req, res) => {
+    res.status(200).json({ message: 'ahi va!' });
+});
+
+index.post('/users/authenticate/', (req, res) => {
+    request.post(
+        'http://localhost:8080/user/login',
+        {
+            json: {
+                email: req.body.email,
+                password: req.body.password,
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'x-clientSecret': 'wolololoaka',
+            },
+        },
+        (error, res2, body) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            console.log(body);
+            res.status(200).json({ jwt: body.token, userName: body.name });
+        },
+    );
+});
+
+module.exports = index;

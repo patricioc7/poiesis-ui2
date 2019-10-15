4b25147b0878d89aaccc39a8ce3d616952ef5a63
Pato <pato@grayshirts.com.ar>
2019-10-15 15:00:47 -0300

    working material login

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,4 +1,19 @@
-import React, { Component } from 'react';
+import React, {useState } from 'react';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 import { SignUpLink } from '../SignUp';
@@ -19,58 +34,146 @@ const INITIAL_STATE = {
     password: '',
     error: null,
 };
-class SignInFormBase extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { ...INITIAL_STATE };
-    }
-    onSubmit = event => {
-        const { email, password } = this.state;
-        this.props.auth.doSignInWithEmailAndPassword
-            (email, password)
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+const SignInFromBaseFunctional = (props) => {
+    const [form, setValues] = useState({...INITIAL_STATE});
+
+    const onSubmit = event => {
+        const { email, password } = form;
+        props.auth.doSignInWithEmailAndPassword
+        (email, password)
             .then(() => {
-                this.setState({ ...INITIAL_STATE });
+                setValues({ ...INITIAL_STATE });
                 console.log('Redireccionando a la home');
-                this.props.history.push(ROUTES.HOME);
+                props.history.push(ROUTES.HOME);
             })
             .catch(error => {
-                this.setState({ error });
+                setValues({ error });
             });
         event.preventDefault();
     };
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+
+    const onChange = event => {
+        setValues({ ...form, [event.target.name]: event.target.value });
     };
-    render() {
-        const { email, password, error } = this.state;
-        const isInvalid = password === '' || email === '';
-        return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="email"
-                    value={email}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    name="password"
-                    value={password}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Password"
-                />
-                <button disabled={isInvalid} type="submit">
-                    Sign In
-                </button>
-                {error && <p>{error.message}</p>}
-            </form>
-        );
-    }
+
+    const { email, password, error } = form;
+    const isInvalid = password === '' || email === '';
+    const classes = useStyles();
+    return(
+
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Bienvenido
+                </Typography>
+                <form className={classes.form} onSubmit={onSubmit} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        value={email}
+                        onChange={onChange}
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        value={password}
+                        onChange={onChange}
+                        label="Contraseña"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Recordarme"
+                    />
+                    <Button  disabled={isInvalid}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Log In
+                    </Button>
+                    {error && <p>{error.message}</p>}
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Olvidaste tu contraseñe?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"No tenés cuenta? Registráte!"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
+    )
+
 }
 const SignInForm = compose(
     withRouter,
     withAuth,
-)(SignInFormBase);
+)(SignInFromBaseFunctional);
 export default SignInPage;
 export { SignInForm };
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
 import { Link , withRouter } from 'react-router-dom';
 import { withAuth } from '../Auth';
 import { compose } from 'recompose';
@@ -18,13 +19,10 @@ const INITIAL_STATE = {
     error: null,
 };
 
-class SignUpFormBase extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { ...INITIAL_STATE };
-    }
+const SignUpFormBaseFunctional = () => {
+    const [form, setValues] = useState({...INITIAL_STATE});
 
-    onSubmit = (event) => {
+    const onSubmit = (event) => {
         const { username, email, passwordOne } = this.state;
         this.props.auth
             .doCreateUserWithEmailAndPassword(username, email, passwordOne)
@@ -38,64 +36,52 @@ class SignUpFormBase extends Component {
         event.preventDefault();
     }
 
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+    const onChange = event => {
+        setValues({ ...form, [event.target.name]: event.target.value });
     };
-    render() {
-        const {
-            username,
-            email,
-            passwordOne,
-            passwordTwo,
-            error,
-        } = this.state;
 
-        const isInvalid =
-            passwordOne !== passwordTwo ||
-            passwordOne === '' ||
-            email === '' ||
-            username === '';
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                name="username"
+                value={username}
+                onChange={onChange}
+                type="text"
+                placeholder="Full Name"
+            />
+            <input
+                name="email"
+                value={email}
+                onChange={onChange}
+                type="text"
+                placeholder="Email Address"
+            />
+            <input
+                name="passwordOne"
+                value={passwordOne}
+                onChange={onChange}
+                type="password"
+                placeholder="Password"
+            />
+            <input
+                name="passwordTwo"
+                value={passwordTwo}
+                onChange={onChange}
+                type="password"
+                placeholder="Confirm Password"
+            />
+            <button disabled={isInvalid} type="submit">Sign Up</button>
+            {error && <p>{error.message}</p>}
+        </form>
+    );
 
-        return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="username"
-                    value={username}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Full Name"
-                />
-                <input
-                    name="email"
-                    value={email}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    name="passwordOne"
-                    value={passwordOne}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Password"
-                />
-                <input
-                    name="passwordTwo"
-                    value={passwordTwo}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Confirm Password"
-                />
-                <button disabled={isInvalid} type="submit">Sign Up</button>
-                {error && <p>{error.message}</p>}
-            </form>
-        );
-    }
 }
+
+
 const SignUpForm = compose(
     withRouter,
     withAuth,
-)(SignUpFormBase);
+)(SignUpFormBaseFunctional);
 
 
 const SignUpLink = () => (
